9f518a3ec49feb7c312100ecb43e60c682a1e69a
Micah Levason <contact@micahlevason.com>
2023-02-13 08:19:58 -0800

    added the services page.

---
+++ src/js/services.js
@@ -0,0 +1,15 @@
+var options = {
+  strings: [
+    "Empowering children to reach their full potential and live their best lives through a personalized and compassionate approach to therapy.",
+    "Providing a culturally-sensitive and inclusive environment for children and adolescents to explore their unique journeys towards growth and development.",
+    "Fostering a safe and supportive space for children and adolescents to heal, learn, and thrive through evidence-based and strengths-focused therapy.",
+  ],
+  typeSpeed: 60,
+  smartBackspace: true, // Default value
+  loop: false,
+  loopCount: Infinity,
+  showCursor: false,
+  cursorChar: "",
+  autoInsertCss: false
+};
+var typed = new Typed(".typedDescAbout", options);
