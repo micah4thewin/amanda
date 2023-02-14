48549704e77247f6b110ba006b4493146743439c
Micah Levason <contact@micahlevason.com>
2023-02-14 12:12:40 -0800

    Added a contact page and resources page.

--- src/js/about.js
+++ src/js/about.js
@@ -1,8 +1,8 @@
 var options = {
   strings: [
-    "Celebrating diversity and promoting acceptance for all members of the LGBTQ+ community.",
-    "Creating a judgment-free environment for LGBTQ+ individuals to thrive.",
-    "Supporting the unique journey of each LGBTQ+ individual towards self-discovery and fulfillment.",
+    "Hello, I'm Amanda Watson, a licensed therapist providing compassionate support to individuals from a diverse and inclusive community. Thank you for visiting my website.",
+    "If you're struggling with thoughts of suicide, concerned about someone close to you, or just need emotional support, I encourage you to seek help. The National Suicide Prevention Lifeline is available 24/7 at 988.",
+    "Please note that I am not currently accepting new clients through my website. If you have any questions or would like more information about the services I offer, please don't hesitate to contact me.",
   ],
   typeSpeed: 60,
   smartBackspace: true, // Default value
---
+++ src/js/contact.js
@@ -0,0 +1,15 @@
+var options = {
+  strings: [
+    "Inclusively supporting unique journeys.",
+    "Empowering through compassionate therapy.",
+    "Creating a safe, supportive space for healing and growth.",
+  ],
+  typeSpeed: 60,
+  smartBackspace: true, // Default value
+  loop: false,
+  loopCount: Infinity,
+  showCursor: false,
+  cursorChar: "",
+  autoInsertCss: false
+};
+var typed = new Typed(".typedDescAbout", options);
